788db1a7b1e1218f240cb4c06413a713a4267339
aryue1945 <aryue1945@gmail.com>
2018-12-17 16:21:46 -0800

    model

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRespository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+    class ProductRespository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products = new List<Product>();
+
+        public ProductRespository()
+        {
+            products = cache["products"] as List<Product>;
+            if (products == null)
+                products = new List<Product>();
+
+        }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+        public void Update(Product p)
+        {
+            Product res = products.Find(x => x.Id == p.Id);
+            if (res == null)
+                throw new Exception("Product not found!");
+            res = p;
+            
+        }
+        public Product Find(string Id)
+        {
+            Product res = products.Find(x => x.Id == Id);
+            if (res == null)
+                throw new Exception("Product not found!");
+            return res;
+        }
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+        public void Delete(string Id)
+        {
+            Product res = products.Find(x => x.Id == Id);
+            if (res == null)
+                throw new Exception("Product not found!");
+            products.Remove(res);
+        }
+
+    }
+}
